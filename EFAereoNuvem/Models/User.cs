fe9323ab159900cf9f586b30e00077d17890a7ab
Yuliangel herrera <yuliangel01herrera@gmail.com>
2025-10-31 21:01:48 -0300

    Autenticação

--- EFAereoNuvem/Models/User.cs
+++ EFAereoNuvem/Models/User.cs
@@ -1,4 +1,5 @@
 ﻿using System.Data;
+using System.Text.Json.Serialization;
 
 namespace EFAereoNuvem.Models;
 public class User
@@ -6,10 +7,11 @@ public class User
     // tabela para controle de acesso (login, senha, permissões).
     public Guid Id { get; set; } = Guid.NewGuid();
     public string Username { get; set; } = string.Empty;
-    public string PasswordHash { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public bool IsActive { get; set; } = true;
+    [JsonIgnore]
+    public string PasswordHash { get; set; } = string.Empty;
 
     // Relação com Role N:N
-    public ICollection<Role> Roles { get; set; } = [];
+    public IList<Role> Roles { get; set; } = [];
 }
